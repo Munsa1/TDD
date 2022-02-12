e89febb13d81232213e4b1d3bf6095661541c5a5
phelian23 <femikola23@gmail.com>
2022-02-12 14:00:11 +0400

    Add test cases for factorial

---
+++ spec/solver_spec.rb
@@ -0,0 +1,23 @@
+require_relative '../solver'
+
+describe Solver do
+  context 'Testing the Solver class' do
+    solver = Solver.new
+    it 'Factorial of 0 returns 1' do
+      expect(solver.factorial(0)).to eq(1)
+    end
+
+    it 'Raise an exception when negative numbers are passed' do
+      expect(solver.factorial(-4)).to eq('No negative numbers allowed')
+    end
+
+    it 'Calculate factorial of positive integers' do
+      expect(solver.factorial(8)).to eq(40_320)
+      expect(solver.factorial(3)).to eq(6)
+    end
+
+    it 'Reverses a string with the reverse method' do
+      expect(solver.reverse)
+    end
+  end
+end
